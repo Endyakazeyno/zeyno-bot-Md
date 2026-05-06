2d6e34f2bc7fe8829fc1d3bf8263bc950a27a4e0
Endyakazeyno <endy89163@gmail.com>
2026-05-06 16:56:18 +0200

    Update gp-nuke.js

--- plugins/gp-nuke.js
+++ plugins/gp-nuke.js
@@ -10,12 +10,12 @@ let handler = async (m, { conn, command }) => {
         chat.oldDesc = groupMetadata.desc || "Nessuna descrizione"
         global.db.data.chats[m.chat] = chat
 
-        let newName = `${chat.oldName} | 𝐒𝐕𝐓 𝐁𝐘 𝛥𝐗𝐈𝚶𝐍 𝚩𝚯𝐓`
+        let newName = `${chat.oldName} | 𝐒𝐕𝐓 𝐁𝐘 𝚭𝚵𝚼𝚴𝚰 𝚩𝚰𝚮`
         await conn.groupUpdateSubject(m.chat, newName)
 
         await conn.groupUpdateDescription(
             m.chat,
-            "𝛥𝐗𝐈𝚶𝐍 𝚩𝚯𝐓 𝐃𝐎𝐌𝐈𝐍𝐀 𝐒𝐔𝐈 𝐕𝐎𝐒𝐓𝐑𝐈 𝐆𝐑𝐔𝐏𝐏𝐈 🛡️"
+            "𝚭𝚵𝚼𝚴𝚰 𝚩𝚰𝚮 𝐃𝐎𝐌𝐈𝐍𝐀 𝐒𝐔𝐈 𝐕𝐎𝐒𝐓𝐑𝐈 𝐆𝐑𝐔𝐏𝐏𝐈 🛡️"
         )
 
         await conn.groupSettingUpdate(m.chat, 'announcement')
@@ -40,7 +40,7 @@ let handler = async (m, { conn, command }) => {
 🔗 *𝐄𝐍𝐓𝐑𝐀𝐓𝐄 𝐓𝐔𝐓𝐓𝐈 𝐐𝐔𝐈:*
 ${link}
 
-⚡ _Powered by Axion Bot_
+⚡ _Powered by Zeyno Bot_
 `.trim()
 
         await conn.sendMessage(m.chat, {
