40b3818aea3d587c7ea2314b71520b04ac44ab14
Endyakazeyno <endy89163@gmail.com>
2026-05-06 16:50:26 +0200

    Update info-staff.js

--- plugins/info-staff.js
+++ plugins/info-staff.js
@@ -7,29 +7,29 @@ let handler = async (m, { conn }) => {
 *╰───────────────╯*
 
 *╭─── 👑 𝐂𝐑𝐄𝐀𝐓𝐎𝐑𝐄 ───╮*
-*│ ✦ 𝐍𝐨𝐦𝐞:* Deadly
+*│ ✦ 𝐍𝐨𝐦𝐞:* Endy
 *│ ✦ 𝐑𝐮𝐨𝐥𝐨:* Creatore / Developer
-*│ ✦ 𝐂𝐨𝐧𝐭𝐚𝐭𝐭𝐨:* @393780306700
+*│ ✦ 𝐂𝐨𝐧𝐭𝐚𝐭𝐭𝐨:* @393501989497
 *╰────────────────────╯*
 
 *╭─── 🔱 𝐂𝐎-𝐎𝐖𝐍𝐄𝐑 ───╮*
-*│ ✦ 𝐁𝐨𝐧𝐳𝐢𝐧𝐨*
+*│ ✦ Medalis*
 *│   ├ 𝐑𝐮𝐨𝐥𝐨:* Co-Owner/ Lead Developer
-*│   └ 𝐂𝐨𝐧𝐭𝐚𝐭𝐭𝐨:* @639350468907
+*│   └ 𝐂𝐨𝐧𝐭𝐚𝐭𝐭𝐨:* @212693877842
 *╰────────────────────╯*
 
 *╭─── 🛡️ 𝐒𝐓𝐀𝐅𝐅 ───╮*
-*│ ✦ 𝐋𝐮𝐱𝐢𝐟𝐞𝐫*
+*│ ✦ zak*
 *│   ├ 𝐑𝐮𝐨𝐥𝐨:* Staffer
-*│   └ 𝐂𝐨𝐧𝐭𝐚𝐭𝐭𝐨:* @212781816909
+*│   └ 𝐂𝐨𝐧𝐭𝐚𝐭𝐭𝐨:* @212612624296
 *╰────────────────────╯*
 
 *╭─── 📌 𝐈𝐍𝐅𝐎 𝐔𝐓𝐈𝐋𝐈 ───╮*
-*│ ✦ 𝐆𝐢𝐭𝐇𝐮𝐛:* github.com/axion-bot
-*│ ✦ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐨:* @393509594333
+*│ ✦ 𝐆𝐢𝐭𝐇𝐮𝐛:* github.com/zeyno-bot-Md
+*│ ✦ 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐨:* @212614898801
 *╰────────────────────╯*
 
-> *𝛥𝐗𝐈𝚶𝐍 𝚩𝚯𝐓*`
+> *𝚭𝚵𝚼𝚴𝚰 𝚩𝚰𝚮*`
 
     await conn.reply(
         m.chat,
@@ -38,10 +38,10 @@ let handler = async (m, { conn }) => {
         {
             contextInfo: {
                 mentionedJid: [
-                    '393780306700@s.whatsapp.net',
-                    '212781816909@s.whatsapp.net',
-                    '639350468907@s.whatsapp.net',
-                    '393509594333@s.whatsapp.net'
+                    '393501989497@s.whatsapp.net',
+                    '212693877842@s.whatsapp.net',
+                    '212612624296@s.whatsapp.net',
+                    '212614898801@s.whatsapp.net'
                 ]
             }
         }
@@ -52,26 +52,26 @@ let handler = async (m, { conn }) => {
             contacts: [
                 {
                     vcard: `BEGIN:VCARD
-VERSION:3.0
-FN:Deadly
-ORG:𝛥𝐗𝐈𝚶𝐍 𝚩𝚯𝐓 - Creatore / Dev
+VERSION:5.0
+FN:Endy
+ORG:𝚭𝚵𝚼𝚴𝚰 𝚩𝚰𝚮 - Creatore / Dev
 TEL;type=CELL;type=VOICE;waid=393780306700:393780306700
 END:VCARD`
                 },
                 {
                     vcard: `BEGIN:VCARD
-VERSION:3.0
-FN:Bonzino
-ORG:𝛥𝐗𝐈𝚶𝐍 𝚩𝚯𝐓 - Co-Owner
-TEL;type=CELL;type=VOICE;waid=639350468907:+639350468907
+VERSION:5.0
+FN: Medalis 
+ORG:𝚭𝚵𝚼𝚴𝚰 𝚩𝚰𝚮 - Co-Owner
+TEL;type=CELL;type=VOICE;waid=212693877842:+212693877842
 END:VCARD`
                 },
                 {
                     vcard: `BEGIN:VCARD
-VERSION:3.0
-FN:Luxifer
-ORG:𝛥𝐗𝐈𝚶𝐍 𝚩𝚯𝐓 - Staffer
-TEL;type=CELL;type=VOICE;waid=212781816909:+212781816909
+VERSION:5.0
+FN:Zak
+ORG:𝚭𝚵𝚼𝚴𝚰 𝚩𝚰𝚮 - Staffer
+TEL;type=CELL;type=VOICE;waid=212612624296:+212612624296
 END:VCARD`
                 }
             ]
